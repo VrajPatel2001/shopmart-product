1aa684ebf8eea063f15be1001314fc7735989e56
Vraj Patel <vrajnp2001@gmail.com>
2023-02-07 21:28:15 -0500

    Table created

--- src/main/java/com/shopmart/productservice/controller/ProductResource.java
+++ src/main/java/com/shopmart/productservice/controller/ProductResource.java
@@ -1,10 +1,15 @@
 package com.shopmart.productservice.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shopmart.productservice.model.Product;
 import com.shopmart.productservice.source.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -18,6 +23,15 @@ public class ProductResource {
      public List<Product> retrieveAllProducts() {
          return productRepository.findAll();
      }
+
+     @PostMapping(value = "/product",consumes = "multipart/form-data")
+    public Product createProduct(@RequestParam("encodedImage") String encodedImage, @RequestParam("product") String productData) throws JsonProcessingException {
+         ObjectMapper mapper = new ObjectMapper();
+         Product product = mapper.readValue(productData, Product.class);
+
+
+         return productRepository.save(product);
+    }
 
 
 }
