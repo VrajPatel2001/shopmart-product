e1e21dfe6a2b7b0d9e978b5d168b8b044026ed83
Vraj Patel <vrajnp2001@gmail.com>
2023-02-07 23:52:14 -0500

    Product controller completed with the database and firebase

--- src/main/java/com/shopmart/productservice/ProductServiceApplication.java
+++ src/main/java/com/shopmart/productservice/ProductServiceApplication.java
@@ -1,12 +1,22 @@
 package com.shopmart.productservice;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
--- src/main/java/com/shopmart/productservice/controller/ProductResource.java
+++ src/main/java/com/shopmart/productservice/controller/ProductResource.java
@@ -2,16 +2,32 @@ package com.shopmart.productservice.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.Credentials;
+import com.google.cloud.storage.*;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.cloud.StorageClient;
 import com.shopmart.productservice.model.Product;
 import com.shopmart.productservice.source.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import java.io.*;
+
+import java.util.Base64;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 @RestController
 public class ProductResource {
@@ -19,15 +35,34 @@ public class ProductResource {
     @Autowired
     private ProductRepository productRepository;
 
-     @GetMapping("/products")
+    @Value("${FIREBASE_APPLICATION_CREDENTIAL}")
+    String path;
+
+    @GetMapping("/products")
      public List<Product> retrieveAllProducts() {
          return productRepository.findAll();
      }
 
      @PostMapping(value = "/product",consumes = "multipart/form-data")
-    public Product createProduct(@RequestParam("encodedImage") String encodedImage, @RequestParam("product") String productData) throws JsonProcessingException {
+    public Product createProduct(@RequestParam("encodedImage") String encodedImage, @RequestParam("product") String productData) throws IOException {
+
          ObjectMapper mapper = new ObjectMapper();
          Product product = mapper.readValue(productData, Product.class);
+
+         byte[] decodedBytes = Base64.getDecoder().decode(encodedImage);
+         InputStream inputStream = new ByteArrayInputStream(decodedBytes);
+
+         Credentials credentials = GoogleCredentials.fromStream(new FileInputStream(path));
+         Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+
+         BlobId blobId = BlobId.of("e-commerce-73720.appspot.com", UUID.randomUUID().toString());
+         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("image/jpeg").build();
+
+         Blob blob = storage.create(blobInfo, inputStream.readAllBytes());
+         String imageUrl = blob.getMediaLink();
+
+            product.setImageURL(imageUrl);
+            System.out.println("Product: " + product);
 
 
          return productRepository.save(product);
